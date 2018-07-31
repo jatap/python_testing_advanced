ad0a8b906d2b35d870cac6c8f5ee71927c19b605
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2018-07-31 18:39:22 +0200

    Bump version: 0.1.2 → 0.1.3

--- python_testing_advanced/__init__.py
+++ python_testing_advanced/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Julio Antúnez Tarín"""
 __email__ = 'julio.antunez.tarin@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jatap/python_testing_advanced',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
